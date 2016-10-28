d5678f00c32e26e6b3485eb30667c487f9b62c41
Brandon Buchanan <willjasen@gmail.com>
2016-10-28 13:51:28 -0400

    create the user and mailbox, then migrate it to Office 365

--- SetupUsers.ps1
+++ SetupUsers.ps1
@@ -1,6 +1,12 @@
-# Migrate a mailbox from on-premise Exchange to Office 365
+# Sets up a new user
+# -Create user in Active Directory
+# -Create on-premise Exchange mailbox
+# -Migrate a mailbox from on-premise Exchange to Office 365
 
+Import-Module ActiveDirectory
 Import-Module MSOnline
+
+$Users = Import-Csv -Path $args[0]
 
 $O365Username = ""
 $O365Password = ""
@@ -8,10 +14,9 @@ $O365Password = ""
 $OnPremUsername = ""
 $OnPremPassword = ""
 
-$MailboxAliasToMove = ""
 $Domain = ""
-$UserPrincipalName = $MailboxAliasToMove + "@" + $Domain
 $RemoteHostName = ""
+$MailboxDatabase = ""
 $TargetDeliverySubDomain = ""
 $TargetDeliveryDomain = $TargetDeliverySubDomain + ".mail.onmicrosoft.com"
 
@@ -24,19 +29,40 @@ $SESSION = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri ht
 Import-PSSession $SESSION
 Connect-MsolService -Credential $O365CREDS
 
-New-MoveRequest -Identity $MailboxAliasToMove -Remote -RemoteHostName $RemoteHostName -TargetDeliveryDomain $TargetDeliveryDomain -RemoteCredential $OnPremCREDS -BadItemLimit 100
+ForEach($User in $Users) {
+  $Username = $User.Username
+  $DisplayName = $User.DisplayName
+  $UserPrincipalName = $User.UserPrincipalName
+  $GivenName = $User.GivenName
+  $Surname = $User.Surname
+  $Department = $User.Department
+  $Title = $User.Title
+  $Company = $User.Company
+  $Path = $User.Path
+  $Password = $User.Password
+  $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+  $ChangePasswordAtLogon = $True
+  $PasswordNeverExpires = $False
 
-Do
-{
-  Start-Sleep -s 10
-  Write-Host "Migration not completed yet.. still waiting..."
-  $Status = Get-MoveRequest | Where-Object {$_.Alias -like $MailboxAliasToMove} | Select Status | Select -Expand Status
+  New-ADUser -Name $DisplayName -UserPrincipalName $UserPrincipalName -SamAccountName $Username -GivenName $GivenName -DisplayName `
+    $DisplayName -SurName $Surname -Title $Title -Company $Company -Path $Path -AccountPassword $SecurePassword `
+  	-Enabled $True -PasswordNeverExpires $PasswordNeverExpires -ChangePasswordAtLogon $ChangePasswordAtLogon
+
+  Enable-Mailbox $UserPrincipalName -Database $MailboxDatabase
+
+  New-MoveRequest -Identity $Username -Remote -RemoteHostName $RemoteHostName -TargetDeliveryDomain $TargetDeliveryDomain -RemoteCredential $OnPremCREDS -BadItemLimit 100
+
+  Do
+  {
+    Start-Sleep -s 30
+    Write-Host "Migration not completed yet.. still waiting..."
+    $Status = Get-MoveRequest | Where-Object {$_.Alias -like $MailboxAliasToMove} | Select Status | Select -Expand Status
+  }
+  While($Status -ne "Completed")
+  Write-Host "Migration completed for $GivenName $Surname"
+
+  Set-MsolUser -UserPrincipalName $UserPrincipalName -UsageLocation US
+  Set-MsolUserLicense -UserPrincipalName $UserPrincipalName -AddLicenses ""
 }
-While($Status -ne "Completed")
 
-Write-Host "Migration completed."
-
-Get-MoveRequest | Where-Object {$_.Alias -like $MailboxAliasToMove}
-
-Set-MsolUser -UserPrincipalName $UserPrincipalName -UsageLocation US
-Set-MsolUserLicense -UserPrincipalName $UserPrincipalName -AddLicenses ""
+Write-Host "Users have been created."
